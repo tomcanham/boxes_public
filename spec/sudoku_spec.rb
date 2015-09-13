091f261e3ff61112a27ee25feedbaec961818f30
Tom Canham <tom@arcadianproductions.com>
2015-09-13 16:17:42 -0700

    Reformat expectation line so it looks prettier

--- spec/sudoku_spec.rb
+++ spec/sudoku_spec.rb
@@ -128,7 +128,8 @@ RSpec.describe Board do
       end while solved.any?
 
       expect(remaining).to be_empty
-      expect(total_solved).to eq(["B6 = 7", "B7 = 5", "C6 = 2", "D5 = 6", "E5 = 4", "E6 = 9", "E8 = 1", "F1 = 2", "F2 = 1",
+      expect(total_solved).to eq([
+        "B6 = 7", "B7 = 5", "C6 = 2", "D5 = 6", "E5 = 4", "E6 = 9", "E8 = 1", "F1 = 2", "F2 = 1",
         "F9 = 6", "G7 = 1", "H3 = 4", "H4 = 1", "H9 = 2", "J9 = 9", "B5 = 1", "C3 = 6", "D4 = 7",
         "D8 = 9", "D9 = 8", "E2 = 8", "E9 = 5", "F8 = 7", "G3 = 8", "G5 = 2", "D1 = 3", "A1 = 5",
         "A2 = 3", "A4 = 6", "A8 = 2", "B2 = 9", "B4 = 3", "B9 = 4", "C2 = 7", "C4 = 5", "C8 = 3",
